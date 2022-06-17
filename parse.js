95e27f79b72cf44319bc02b983eb19c1f8978012
Theo Moustakis <theo.moustakis@capgemini.com>
2022-06-17 15:26:14 +0100

    move output

--- parse.js
+++ parse.js
@@ -1,5 +1,4 @@
 
-// __dirname means relative to script. Use "./data.txt" if you want it relative to execution path.
 var fs = require('fs');
 var array = fs.readFileSync('Jenkinsfile').toString().split("\n");
 var yaml = fs.readFileSync('template.yml').toString().split("\n");
@@ -9,9 +8,8 @@ for(i in array) {
         i++;
 
         console.log(array[i]);
-        //fs.writeFile(yaml.toString().replace(/{{ command }}/, array[i]));
+        
         console.log(yaml.toString().replace(/{{ command }}/, array[i]));
-        //var file = fs.createWriteStream('output.yml');
-        //console.log(array[i]);
+       
     }
 }
